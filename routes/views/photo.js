a7e13beeb0772d1101f2960c6b2191dd2ff3b621
oizhaolei <oizhaolei@gmail.com>
2015-07-29 16:51:11 +0800

    photo

--- routes/views/photo.js
+++ routes/views/photo.js
@@ -1,4 +1,5 @@
 "use strict";
+var logger = require('log4js').getLogger('routes/vies/photo.js');
 var keystone = require('keystone'),
     _ = require('underscore');
 
@@ -9,9 +10,20 @@ exports = module.exports = function(req, res) {
 };
 
 
+var UserPhoto = keystone.list('UserPhoto');
+
 exports.show = function(req, res, next){
   var photo_id = req.params.photo_id;
-  res.json( {
-    photo_id : photo_id
-  });
+  var view = new keystone.View(req, res),
+      locals = res.locals;
+  logger.info(photo_id);
+  UserPhoto.model.findById(photo_id)
+    .exec(function(err, userPhoto) {
+      locals.userPhoto = userPhoto;
+      locals.userPhoto.populateRelated('comments', function() {
+        logger.info(userPhoto);
+        view.render('site/photo');
+      });
+
+    });
 };
